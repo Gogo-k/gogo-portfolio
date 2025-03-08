b7b7fadde28341f780f8ca18be2a25de59eab789
Gogo <gogoskole@gmail.com>
2025-03-08 15:30:45 +0100

    Vercel deploy, on github push test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       
       <h1 className="text-4xl font-bold text-center sm:text-5xl">
-        Hello World!
+        Test to see if website is deployed on Vercel, after pushing to GitHub.
       </h1>
       
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
